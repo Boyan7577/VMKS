076cf8f9f8e62bc2c484779eabf165e2415f2196
Boyan7577 <107467830+Boyan7577@users.noreply.github.com>
2024-03-31 15:24:01 +0300

    Add files via upload

--- BuzzerTest.ino
+++ BuzzerTest.ino
@@ -1,13 +1,24 @@
-const int buzzer = 11;
+const int flamepin=A2;
+const int buzpin=11;
+const int threshold=200;// sets threshold value for flame sensor
+int flamesensvalue=0; // initialize flamesensor reading
 
-void setup()
+void setup() 
 {
-  pinMode(buzzer, OUTPUT);
+  Serial.begin(9600);
+  pinMode(flamepin,INPUT);
+  pinMode(buzpin,OUTPUT);
 }
-void loop()
-{
-  tone(buzzer, 1000); // 1KHz
-  delay(1000);       
-  noTone(buzzer);     // Stop
-  delay(1000);      
-}   
+
+void loop() {
+  flamesensvalue=analogRead(flamepin); // reads analog data from flame sensor
+  if (flamesensvalue<=threshold) 
+  { // compares reading from flame sensor with the threshold value
+    tone(buzpin,100);
+    delay(1000); //stops program for 1 second
+  }
+  else
+  {
+    noTone(buzpin);
+  }
+}
